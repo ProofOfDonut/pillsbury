349f8c3b8d2a28bc8c9bd8cdc89954aeb4395da5
Dan Donut <proofofdonut@gmail.com>
2019-04-17 14:25:14 +0000

    Fixed number formatter to work on negative numbers

--- common/numbers/format.ts
+++ common/numbers/format.ts
@@ -1,13 +1,15 @@
 import {ensure} from '../ensure';
 
 export function formatNumber(n: number): string {
-  const s = String(n);
-  ensure(/^\d+$/.test(s), `Cannot format number ({n}).`);
+  const m = /^(-)?(\d+)$/.exec(String(n));
+  ensure(m, `Cannot format number (${n}).`);
+  const sign = m[1] || '';
+  const numStr = m[2];
   let out = [];
-  for (let i = 0; i < s.length; i += 3) {
-    const p0 = Math.max(0, s.length - i - 3);
-    const p1 = s.length - i;
-    out.unshift(s.slice(p0, p1));
+  for (let i = 0; i < numStr.length; i += 3) {
+    const p0 = Math.max(0, numStr.length - i - 3);
+    const p1 = numStr.length - i;
+    out.unshift(numStr.slice(p0, p1));
   }
-  return out.join(',');
+  return sign + out.join(',');
 }
